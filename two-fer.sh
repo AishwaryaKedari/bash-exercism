5b901d5e094b07e06d83cef2dee810cacf22bb72
AishwaryaKedari <88555775+AishwaryaKedari@users.noreply.github.com>
2024-07-26 22:37:38 -0700

    Update two-fer.sh

--- two-fer.sh
+++ two-fer.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+
+# The following comments should help you get started:
+
 if [ -z "$1" ] 
 then
-  echo "One for you, one for me."
+    echo "One for you, one for me."
 else
-  echo "One for $1, one for me."
+    echo "One for $1, one for me."
 fi
